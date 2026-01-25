d94189a08a06478c8d28a7135851615d8b0a82ff
RohitMunjal25 <mobilemunjal@gmail.com>
2026-01-25 22:12:23 +0530

    final commit

--- app.py
+++ app.py
@@ -66,12 +66,6 @@ def admin():
         subscribers=subscribers,
         chat_logs=chat_logs
     )
-
-@app.route("/health", methods=["GET"])
-def health():
-    return jsonify({"status": "alive"})
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
