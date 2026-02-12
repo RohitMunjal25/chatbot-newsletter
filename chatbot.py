653bba2e36c255a7fafaa1dba7a17205a3708b32
RohitMunjal25 <mobilemunjal@gmail.com>
2026-02-12 10:29:21 +0530

    chatbot changes

--- chatbot.py
+++ chatbot.py
@@ -4,8 +4,12 @@ with open("dataset.json") as f:
     DATA = json.load(f)["faq"]
 
 def find_answer(user_msg):
-    user_msg = user_msg.lower()
+    user_msg = user_msg.strip().lower()
+
     for item in DATA:
-        if item["question"] in user_msg:
+        question = item["question"].strip().lower()
+
+        if question == user_msg:
             return item["answer"]
+
     return None
